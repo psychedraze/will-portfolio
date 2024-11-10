8520b93659aac30f6f49fc201d90896e1ff27a25
psychedraze <psychedraze@gmail.com>
2024-11-10 14:16:08 -0700

    links table update

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,9 +1,9 @@
 export const LINKS = {
-  github: "https://github.com/Ladvace",
-  linkedin: "https://www.linkedin.com/in/gianmarco-cavallo/",
-  medium: "https://ladvace.medium.com/",
-  discord: "https://discordapp.com/users/163300027618295808",
-  dribble: "https://dribbble.com/Ladvace_Jace",
+  // github: "https://github.com/Ladvace",
+  // linkedin: "https://www.linkedin.com/in/gianmarco-cavallo/",
+  // medium: "https://ladvace.medium.com/",
+  discord: "https://discordapp.com/users/467621913330581504",
+  // dribble: "https://dribbble.com/Ladvace_Jace",
 };
 
 export const loaderAnimation = [
